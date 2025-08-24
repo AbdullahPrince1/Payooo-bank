0c4a9b2834e8ae52d32b8b65f88b814608842ab0
Abdullah Prince <princex660@gmail.com>
2025-08-24 16:09:40 +0600

    toggle system complete

--- homePage.js
+++ homePage.js
@@ -1,27 +1,74 @@
 const addMoneyBtn = document.querySelector("#addMoneyBtn");
-const totalAccBalance = document.querySelector("#totalAccBalance");
-const addedAmmount = document.querySelector("#addedAmmount");
-const accNumber = document.querySelector("#accNumber");
-const accPinNumber = document.querySelector("#accPinNumber");
 const customerDetails = [
   {
     customerAccNumber: 123456789,
     customerPin: 1234,
   },
 ];
+// function to get input value
+function getValue(id) {
+  return Number(document.getElementById(id).value);
+}
+// function to get inner text
+function getInnervalue(id) {
+  return Number(document.getElementById(id).textContent);
+}
+// function to set inner text
+function setInnerValue(value) {
+  const balanceAmmount = (document.getElementById(
+    "totalAccBalance"
+  ).textContent = value);
+  return balanceAmmount;
+}
 addMoneyBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   for (const element of customerDetails) {
     if (
-      element.customerAccNumber === Number(accNumber.value) &&
-      element.customerPin === Number(accPinNumber.value)
+      element.customerAccNumber === getValue("accNumber") &&
+      element.customerPin === getValue("accPinNumber")
     ) {
-      const total =
-        Number(addedAmmount.value) + Number(totalAccBalance.textContent);
-      totalAccBalance.textContent = total;
-      return;
+      const total = getValue("addedAmmount") + getInnervalue("totalAccBalance");
+      setInnerValue(total);
     } else {
       alert("Invalid pin or account number");
     }
   }
 });
+//toggle system
+function toggleMenu(id) {
+  const form = document.getElementsByClassName("toggleParent");
+  for (const forms of form) {
+    forms.style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
+document
+  .getElementById("addMoneyElement")
+  .addEventListener("click", function () {
+    toggleMenu("addMoneyParent");
+  });
+document
+  .getElementById("cashOutElement")
+  .addEventListener("click", function () {
+    toggleMenu("cashOutParent");
+  });
+document
+  .getElementById("transferMoneyElement")
+  .addEventListener("click", function () {
+    toggleMenu("transferMoneyParent");
+  });
+document
+  .getElementById("getBonusElement")
+  .addEventListener("click", function () {
+    toggleMenu("getBonusParent");
+  });
+document
+  .getElementById("payBillElement")
+  .addEventListener("click", function () {
+    toggleMenu("payBillParent");
+  });
+// document
+//   .getElementById("transactionElement")
+//   .addEventListener("click", function () {
+//     toggleMenu("getBonusParent");
+//   });
